ea1a7e7ea7a50d7dbf56de27b0be4f638fcefe87
wilsontbx <boonxian1993@hotmail.com>
2021-02-24 14:11:50 +0800

    added post route and get route

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello World");
+  res.status(200).json({
+    0: "GET    /",
+    1: "GET    /jumplings",
+    2: "POST   /jumplings",
+    3: "GET /jumplings/:name",
+    4: "PUT /jumplings/:id",
+    5: "DELETE /jumplings/:id",
+    6: "-----------------------",
+    7: "GET    /jumplings/presenter",
+  });
 });
 
 const router = require("./src/routes");
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require("joi");
+
+//data
+const jumplings = [];
+
+//validation
+function validate(jump) {
+  const schema = Joi.object({
+    id: Joi.number().integer(),
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(jump);
+}
+
+//route
+router.get("/", (req, res) => {
+  res.status(200).json(jumplings);
+});
+
+router.post("/", (req, res, next) => {
+  const validation = validate(req.body);
+  if (validation.error) {
+    const error = new Error(validation.error.details[0].message);
+    error.statusCode = 400;
+    next(error);
+  }
+  const newJump = {
+    id: jumplings.length + 1,
+    name: req.body.name,
+  };
+  jumplings.push(newJump);
+  res.status(200).json(newJump);
+});
+
+//validation
+router.use((err, req, res, next) => {
+  res.statusCode = err.statusCode;
+  res.send(`${err}`);
+});
 
 module.exports = router;
