b08fd0f1304770127b135d3f8d14db57b05b46f2
wilsontbx <boonxian1993@hotmail.com>
2021-03-30 15:49:30 +0800

    restuture test code

--- __test__/users.test.js
+++ __test__/users.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../src/app");
-const UserModel = require("../src/models/user.model");
 const dbHandlers = require("../test/dbHandler");
+const UserModel = require("../src/models/user.model");
 
 describe("Route", () => {
   beforeAll(async () => {
