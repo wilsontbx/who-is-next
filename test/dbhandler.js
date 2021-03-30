14dc82452fd3403b9d52cc722faa331a82be03c1
wilsontbx <boonxian1993@hotmail.com>
2021-03-30 15:45:24 +0800

    restruture the dbhandler

--- test/dbhandler.js
+++ test/dbhandler.js
@@ -3,32 +3,32 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 
 const mongod = new MongoMemoryServer();
 
-const connect = async () => {
-  const uri = await mongod.getUri();
+const dbHandlers = {
+  connect: async () => {
+    const uri = await mongod.getUri();
 
-  const mongooseOpts = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  };
+    const mongooseOpts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    };
 
-  await mongoose.connect(uri, mongooseOpts);
+    await mongoose.connect(uri, mongooseOpts);
+  },
+  closeDatabase: async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+  },
+  clearDatabase: async () => {
+    const collections = mongoose.connection.collections;
+
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany();
+    }
+  },
 };
 
-const closeDatabase = async () => {
-  await mongoose.connection.dropDatabase();
-  await mongoose.connection.close();
-  await mongod.stop();
-};
-
-const clearDatabase = async () => {
-  const collections = mongoose.connection.collections;
-
-  for (const key in collections) {
-    const collection = collections[key];
-    await collection.deleteMany();
-  }
-};
-
-module.exports = { connect, closeDatabase, clearDatabase };
+module.exports = dbHandlers;
